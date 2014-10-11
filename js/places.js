be3524c03cf248b2421c2d8aa59767b6c27b2bf8
Johannes Schickling <schickling.j@gmail.com>
2014-10-11 23:36:53 +0200

    fix js

--- js/places.js
+++ js/places.js
@@ -1,47 +1,47 @@
 var Places = {
-	auditorium: {
-		left: "images/auditorium/left.jpg",
-		right: "images/auditorium/right.jpg",
-		name: "Auditorium",
-		frames:[
-				{left:["images/auditorium/single_left.jpg", "images/auditorium/single_left2.jpg"],
-				right:["images/auditorium/single_right.jpg", "images/auditorium/single_right2.jpg"],
-				scaleL:[0.7, 0.7, 0.7],
-				positionL:[6.5, -7.5, 11],
-				rotationL:[-5.8, -2.8, 0.1],
-				scaleR:[0.9, 0.9, 0.89],
-				positionR:[6.5, -9.5, 13.5],
-				rotationR:[-5.7, -2.9, 0]}
-			]
-	},
-	technopark: {
-		left: "images/technopark/left.jpg",
-		right: "images/technopark/right.jpg",
-		name: "Technopark",
-		frames:[]
-	},
-	swisscom: {
-		left: "images/swisscom/left.jpg",
-		right: "images/swisscom/right.jpg",
-		name: "Swisscom",
-		frames:[]
-	},
-	hackzurich: {
-		left: "images/hackzurich/left.jpg",
-		right: "images/hackzurich/right.jpg",
-		name: "Swisscom",
-		frames:[]
-	},
-	belmorepark: {
-		left: "images/belmorepark/left.jpg",
-		right: "images/belmorepark/right.jpg",
-		name: "Belmorepark",
-		frames:[]
-	},
-	technoparknight: {
-		left: "images/technoparknight/left.jpg",
-		right: "images/technoparknight/right.jpg",
-		name: "Technopark Night",
-		frames:[]
-	}
-}
+  auditorium: {
+    left: "images/auditorium/left.jpg",
+    right: "images/auditorium/right.jpg",
+    name: "Auditorium",
+    frames: [{
+      left: ["images/auditorium/single_left.jpg", "images/auditorium/single_left2.jpg"],
+      right: ["images/auditorium/single_right.jpg", "images/auditorium/single_right2.jpg"],
+      scaleL: [0.7, 0.7, 0.7],
+      positionL: [6.5, -7.5, 11],
+      rotationL: [-5.8, -2.8, 0.1],
+      scaleR: [0.9, 0.9, 0.89],
+      positionR: [6.5, -9.5, 13.5],
+      rotationR: [-5.7, -2.9, 0]
+    }]
+  },
+  technopark: {
+    left: "images/technopark/left.jpg",
+    right: "images/technopark/right.jpg",
+    name: "Technopark",
+    frames: []
+  },
+  swisscom: {
+    left: "images/swisscom/left.jpg",
+    right: "images/swisscom/right.jpg",
+    name: "Swisscom",
+    frames: []
+  },
+  hackzurich: {
+    left: "images/hackzurich/left.jpg",
+    right: "images/hackzurich/right.jpg",
+    name: "Swisscom",
+    frames: []
+  },
+  belmorepark: {
+    left: "images/belmorepark/left.jpg",
+    right: "images/belmorepark/right.jpg",
+    name: "Belmorepark",
+    frames: []
+  },
+  technoparknight: {
+    left: "images/technoparknight/left.jpg",
+    right: "images/technoparknight/right.jpg",
+    name: "Technopark Night",
+    frames: []
+  }
+};
