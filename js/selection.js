14c74edce6a2726f2619f808ecfd18f960bc5ab8
Johannes Schickling <schickling.j@gmail.com>
2014-10-12 03:11:02 +0200

    css hover

--- js/selection.js
+++ js/selection.js
@@ -1,6 +1,13 @@
 angular.module('app', [])
   .controller('SelectionController', function($scope) {
 
+    history.pushState(null, document.title, window.location.pathname);
+
     $scope.places = Places;
+
+    $scope.select = function(placeId) {
+      location.hash = '#/' + placeId;
+      location.pathname = location.pathname.split('/').slice(0, -1).join('/');
+    };
 
   });
