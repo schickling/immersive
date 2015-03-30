010bc4b9bd763e9f0698ce0ae2c958e5d306d064
Johannes Schickling <schickling.j@gmail.com>
2015-03-30 23:51:14 +0200

    dessert

--- js/places.js
+++ js/places.js
@@ -1,155 +1,162 @@
 ﻿var Places = {
-  auditorium: {
-    left: "images/auditorium/left.jpg",
-    right: "images/auditorium/right.jpg",
-    name: "Auditorium",
-    frames: [{
-      left: ["images/auditorium/single_left.jpg", "images/auditorium/single_left2.jpg"],
-      right: ["images/auditorium/single_left.jpg", "images/auditorium/single_left2.jpg"],
-      scaleL: [0.6, 0.6, 0.6],
-      positionL: [-60, 2.5, 45],
-      rotationL: [-5.7, -4.49, -0.6],
-      scaleR: [0.6, 0.6, 0.6],
-      positionR: [-49, 4.5, 46.5],
-      rotationR: [-5.7, -4.49, -0.6],
-      aniDelay: 130
-    }],
-    thumbnail: "images/auditorium/thumb.jpg"
-  },
-  dom: {
-    left: "images/dom/domleft.jpg",
-    right: "images/dom/domright.jpg",
-    name: "Aachener Dom",
-    frames: [],
-    thumbnail: "images/dom/thumb.jpg"
-  },
-  technopark: {
-    left: "images/technopark/left.jpg",
-    right: "images/technopark/right.jpg",
-    name: "Technopark",
-    frames: [],
-    thumbnail: "images/technopark/thumb.jpg"
-  },
-  zimmer: {
-    left: "images/zimmer/left.jpg",
-    right: "images/zimmer/right.jpg",
-    name: "Zimmer",
-    frames: [],
-    thumbnail: "images/zimmer/thumb.jpg"
-  },
-  swisscom: {
-    left: "images/swisscom/left.jpg",
-    right: "images/swisscom/right.jpg",
-    name: "Swisscom",
-    frames: [],
-    thumbnail: "images/swisscom/thumb.jpg"
-  },
-  fussballfeld: {
-    left: "images/fussballfeld/left.jpg",
-    right: "images/fussballfeld/right.jpg",
-    name: "Fussballfeld",
-    frames: [],
-    thumbnail: "images/fussballfeld/thumb.jpg"
-  },
-  tamedia: {
-    left: "images/tamedia/left.jpg",
-    right: "images/tamedia/right.jpg",
-    name: "Tamedia Welcome Hall",
-    frames: [],
-    thumbnail: "images/tamedia/thumb.jpg"
-  },
-  //hackzurich: {
-  //  left: "images/hackzurich/left.jpg",
-  //  right: "images/hackzurich/right.jpg",
-  //  name: "Hackzurich",
-  //  frames: [],
-  //  thumbnail: "images/hackzurich/thumb.jpg"
-  //},
-  //belmorepark: {
-  //left: "images/belmorepark/left.jpg",
-  //right: "images/belmorepark/right.jpg",
-  //name: "Belmorepark",
-  //frames: [],
-  //thumbnail: "images/belmorepark/thumb.jpg"
-  //},
-  technoparknight: {
-    left: "images/technoparknight/left.jpg",
-    right: "images/technoparknight/right.jpg",
-    name: "Technopark Night",
-    frames: [],
-    thumbnail: "images/technoparknight/thumb.jpg"
-  },
-  ehrenhof: {
-    left: "images/ehrenhof_rainy/left.jpg",
-    right: "images/ehrenhof_rainy/right.jpg",
-    name: "KIT Ehrenhof",
-    frames: [],
-    thumbnail: "images/ehrenhof_rainy/thumb.jpg"
-  },
-  schloss: {
-    left: "images/schloss/left.jpg",
-    right: "images/schloss/right.jpg",
-    name: "Karlsruhe Schloss",
-    frames: [],
-    thumbnail: "images/schloss/thumb.jpg"
-  },
-  //technoparknight2: {
-  //  left: "images/technoparknight2/left.jpg",
-  //  right: "images/technoparknight2/right.jpg",
-  //  name: "Technopark Night II",
-  //  frames: [],
-  //  thumbnail: "images/technoparknight2/thumb.jpg"
-  //},
-  //hall: {
-  //left: "images/hall/left.jpg",
-  //right: "images/hall/right.jpg",
-  //name: "Strange Hall",
-  //frames: [],
-  //thumbnail: "images/hall/thumb.jpg"
-  //},
-  //schiffbau: {
-  //left: "images/schiffbau/left.jpg",
-  //right: "images/schiffbau/right.jpg",
-  //name: "Schiffbau",
-  //frames: [],
-  //thumbnail: "images/schiffbau/thumb.jpg"
-  //},
-  hardbruecke: {
-    left: "images/hardbruecke/left.jpg",
-    right: "images/hardbruecke/right.jpg",
-    name: "Hardbrücke",
-    frames: [{
-      left: ["images/hardbruecke/ani0.jpg",
-        "images/hardbruecke/ani1.jpg",
-        "images/hardbruecke/ani2.jpg",
-        "images/hardbruecke/ani3.jpg",
-        "images/hardbruecke/ani4.jpg",
-        "images/hardbruecke/ani5.jpg",
-        "images/hardbruecke/ani6.jpg",
-        "images/hardbruecke/ani7.jpg",
-        "images/hardbruecke/ani8.jpg",
-        "images/hardbruecke/ani9.jpg"
-      ],
-      right: ["images/hardbruecke/ani0.jpg",
-        "images/hardbruecke/ani1.jpg",
-        "images/hardbruecke/ani2.jpg",
-        "images/hardbruecke/ani3.jpg",
-        "images/hardbruecke/ani4.jpg",
-        "images/hardbruecke/ani5.jpg",
-        "images/hardbruecke/ani6.jpg",
-        "images/hardbruecke/ani7.jpg",
-        "images/hardbruecke/ani8.jpg",
-        "images/hardbruecke/ani9.jpg"
-      ],
+    auditorium: {
+        left: "images/auditorium/left.jpg",
+        right: "images/auditorium/right.jpg",
+        name: "Auditorium",
+        frames: [{
+            left: ["images/auditorium/single_left.jpg", "images/auditorium/single_left2.jpg"],
+            right: ["images/auditorium/single_left.jpg", "images/auditorium/single_left2.jpg"],
+            scaleL: [0.6, 0.6, 0.6],
+            positionL: [-60, 2.5, 45],
+            rotationL: [-5.7, -4.49, -0.6],
+            scaleR: [0.6, 0.6, 0.6],
+            positionR: [-49, 4.5, 46.5],
+            rotationR: [-5.7, -4.49, -0.6],
+            aniDelay: 130
+        }],
+        thumbnail: "images/auditorium/thumb.jpg"
+    },
+    dom: {
+        left: "images/dom/domleft.jpg",
+        right: "images/dom/domright.jpg",
+        name: "Aachener Dom",
+        frames: [],
+        thumbnail: "images/dom/thumb.jpg"
+    },
+    technopark: {
+        left: "images/technopark/left.jpg",
+        right: "images/technopark/right.jpg",
+        name: "Technopark",
+        frames: [],
+        thumbnail: "images/technopark/thumb.jpg"
+    },
+    zimmer: {
+        left: "images/zimmer/left.jpg",
+        right: "images/zimmer/right.jpg",
+        name: "Zimmer",
+        frames: [],
+        thumbnail: "images/zimmer/thumb.jpg"
+    },
+    swisscom: {
+        left: "images/swisscom/left.jpg",
+        right: "images/swisscom/right.jpg",
+        name: "Swisscom",
+        frames: [],
+        thumbnail: "images/swisscom/thumb.jpg"
+    },
+    fussballfeld: {
+        left: "images/fussballfeld/left.jpg",
+        right: "images/fussballfeld/right.jpg",
+        name: "Fussballfeld",
+        frames: [],
+        thumbnail: "images/fussballfeld/thumb.jpg"
+    },
+    tamedia: {
+        left: "images/tamedia/left.jpg",
+        right: "images/tamedia/right.jpg",
+        name: "Tamedia Welcome Hall",
+        frames: [],
+        thumbnail: "images/tamedia/thumb.jpg"
+    },
+    //hackzurich: {
+    //  left: "images/hackzurich/left.jpg",
+    //  right: "images/hackzurich/right.jpg",
+    //  name: "Hackzurich",
+    //  frames: [],
+    //  thumbnail: "images/hackzurich/thumb.jpg"
+    //},
+    //belmorepark: {
+    //left: "images/belmorepark/left.jpg",
+    //right: "images/belmorepark/right.jpg",
+    //name: "Belmorepark",
+    //frames: [],
+    //thumbnail: "images/belmorepark/thumb.jpg"
+    //},
+    technoparknight: {
+        left: "images/technoparknight/left.jpg",
+        right: "images/technoparknight/right.jpg",
+        name: "Technopark Night",
+        frames: [],
+        thumbnail: "images/technoparknight/thumb.jpg"
+    },
+    ehrenhof: {
+        left: "images/ehrenhof_rainy/left.jpg",
+        right: "images/ehrenhof_rainy/right.jpg",
+        name: "KIT Ehrenhof",
+        frames: [],
+        thumbnail: "images/ehrenhof_rainy/thumb.jpg"
+    },
+    schloss: {
+        left: "images/schloss/left.jpg",
+        right: "images/schloss/right.jpg",
+        name: "Karlsruhe Schloss",
+        frames: [],
+        thumbnail: "images/schloss/thumb.jpg"
+    },
+    dessert: {
+        left: "images/dessert/left.jpg",
+        right: "images/dessert/right.jpg",
+        name: "Dessert",
+        frames: [],
+        thumbnail: "images/dessert/thumb.jpg"
+    },
+    //technoparknight2: {
+    //  left: "images/technoparknight2/left.jpg",
+    //  right: "images/technoparknight2/right.jpg",
+    //  name: "Technopark Night II",
+    //  frames: [],
+    //  thumbnail: "images/technoparknight2/thumb.jpg"
+    //},
+    //hall: {
+    //left: "images/hall/left.jpg",
+    //right: "images/hall/right.jpg",
+    //name: "Strange Hall",
+    //frames: [],
+    //thumbnail: "images/hall/thumb.jpg"
+    //},
+    //schiffbau: {
+    //left: "images/schiffbau/left.jpg",
+    //right: "images/schiffbau/right.jpg",
+    //name: "Schiffbau",
+    //frames: [],
+    //thumbnail: "images/schiffbau/thumb.jpg"
+    //},
+    hardbruecke: {
+        left: "images/hardbruecke/left.jpg",
+        right: "images/hardbruecke/right.jpg",
+        name: "Hardbrücke",
+        frames: [{
+            left: ["images/hardbruecke/ani0.jpg",
+                "images/hardbruecke/ani1.jpg",
+                "images/hardbruecke/ani2.jpg",
+                "images/hardbruecke/ani3.jpg",
+                "images/hardbruecke/ani4.jpg",
+                "images/hardbruecke/ani5.jpg",
+                "images/hardbruecke/ani6.jpg",
+                "images/hardbruecke/ani7.jpg",
+                "images/hardbruecke/ani8.jpg",
+                "images/hardbruecke/ani9.jpg"
+            ],
+            right: ["images/hardbruecke/ani0.jpg",
+                "images/hardbruecke/ani1.jpg",
+                "images/hardbruecke/ani2.jpg",
+                "images/hardbruecke/ani3.jpg",
+                "images/hardbruecke/ani4.jpg",
+                "images/hardbruecke/ani5.jpg",
+                "images/hardbruecke/ani6.jpg",
+                "images/hardbruecke/ani7.jpg",
+                "images/hardbruecke/ani8.jpg",
+                "images/hardbruecke/ani9.jpg"
+            ],
 
-      scaleL: [2.0, 2.0, 2.0],
-      positionL: [-59, 10.0, 40],
-      rotationL: [-6.4, -3.7, 0.1],
-      scaleR: [2.0, 2.0, 2.0],
-      positionR: [-52.5, 11.5, 47],
-      rotationR: [-6.4, -3.7, 0.1],
-      aniDelay: 20
-    }],
-    thumbnail: "images/hardbruecke/thumb.jpg"
-  }
+            scaleL: [2.0, 2.0, 2.0],
+            positionL: [-59, 10.0, 40],
+            rotationL: [-6.4, -3.7, 0.1],
+            scaleR: [2.0, 2.0, 2.0],
+            positionR: [-52.5, 11.5, 47],
+            rotationR: [-6.4, -3.7, 0.1],
+            aniDelay: 20
+        }],
+        thumbnail: "images/hardbruecke/thumb.jpg"
+    }
 }
